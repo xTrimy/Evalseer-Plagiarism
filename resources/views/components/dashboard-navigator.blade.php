36ebca2660053f46d0bf12da939aa99660b318b6
xBodda <abdelrahman3aysh@hotmail.com>
2022-02-25 16:12:02 +0200

    Submission & Navigator

--- resources/views/components/dashboard-navigator.blade.php
+++ resources/views/components/dashboard-navigator.blade.php
@@ -18,7 +18,7 @@
               @endif
               <a
                 class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="#"
+                href="/dashboard/"
               >
                 <svg
                   class="w-5 h-5"
