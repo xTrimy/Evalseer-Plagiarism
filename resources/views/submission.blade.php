ca99ec44dc8328eabd000bdbfb6c2fb4b1eefaf5
xTrimy <Mohamed.ashraf881999@gmail.com>
2022-03-08 08:28:15 +0200

    fix styling feedback bug

--- resources/views/submission.blade.php
+++ resources/views/submission.blade.php
@@ -172,8 +172,11 @@
                     array_splice($styling_line, count($styling_line)-3, 3); 
                     for($i = 0; $i<count($styling_line);$i++){
                         $data = explode(":",$styling_line[$i]);
-						if($data[0] != "Error")
-						$styling_comment[$data[1]] = explode(":",$styling_line[$i])[2];
+						if($data[0] != "Error"){
+                            if(isset($data[1])){
+						        $styling_comment[$data[1]] = explode(":",$styling_line[$i])[2];
+                            }
+                        }
                     }
                 @endphp
             @endif
