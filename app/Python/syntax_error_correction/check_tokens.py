556d149eb4df86002d2e3193073bfa7b33a3766a
xTrimy <Mohamed.ashraf881999@gmail.com>
2022-03-26 01:22:09 +0200

    Add basic manual syntax error detection functions

---
+++ app/Python/syntax_error_correction/check_tokens.py
@@ -0,0 +1,17 @@
+import main as tokenizer
+import sys
+import json
+
+
+
+if __name__ == "__main__":
+    check_for_array = ["return in main"]
+    for feature in check_for_array:
+        check_for_return = tokenizer.token_checker(check_for=feature)
+        if(not check_for_return):
+            json_data = {"status":"warning","checker":"return in main", "found":"false"}
+            print(json.dumps(json_data))
+            exit(0)
+    
+    json_data = {"status":"success"}
+    print(json.dumps(json_data))
--- app/Python/syntax_error_correction/tests/test.cpp
+++ app/Python/syntax_error_correction/tests/test.cpp
@@ -1,11 +1,50 @@
-# include <iostream>
- using std :: cin ;
- using std :: cout ;
- int multiply ( int x ) {
- return x * 2 ;
+# include < iostream >
+ using namespace std ; 
+void swap ( int * a , int * b ) { int t = * a ; 
+* a = * b ; 
+* b = t ; 
+} void printArray ( int array [ ] , int size ) { int i ; 
+for ( i = 0 ; 
+i < size ; 
+i ++ ) cout << array [ i ] << " " ;
+ cout <<endl ;
  }
+ int partition ( int array [ ] , int low , int high ) {
+ int pivot = array [ high ] ;
+ int i = ( low - 1 ) ;
+ for ( int j = low ;
+ j <high ;
+ j ++ ) {
+ if ( array [ j ] <= pivot ) {
+ i ++ ;
+ swap ( & array [ i ] , & array [ j ] ) ;
+ }
+ }
+ swap ( & array [ i + 1 ] , & array [ high ] ) ;
+ return ( i + 1 ) ;
+ }
+ void quickSort ( int array [ ] , int low , int high ) {
+ if ( low <high ) {
+ int pi = partition ( array , low , high ) ;
+ quickSort ( array , low , pi - 1 ) ;
+ quickSort ( array , pi + 1 , high ) ;
+ }
+ }
+ int binarySearch ( int array [ ] , int x , int low , int high ) {
+ quickSort ( array , low , high ) ;
+ while ( low <= high ) {
+ int mid = low + ( high - low ) 2 ;
+ if ( array [ mid ] == x ) return mid ;
+ if ( array [ mid ] <x ) low = mid + 1 ;
+ else high = mid - 1 ;
+ }
+ return - 1 }
  int main ( ) {
- int x ;
- cin>> x ;
- cout <<multiply ( x ) ;
+ int arr [ ] = {
+ 2 , 6 , 5 , 7 , 11 , 4 }
+ ;
+ int n ;
+ cin>> n ;
+ cout <<binarySearch ( arr , n , 0 , 4 ) ;
+ & 0 ;
  }
