a14673043797b4f59f9991f98f5ed3be2b6bc032
xBodda <abdelrahman3aysh@hotmail.com>
2022-05-27 22:28:13 +0200

    Adding Dashboard Name

--- resources/views/layout/dashboard/app.blade.php
+++ resources/views/layout/dashboard/app.blade.php
@@ -1,6 +1,8 @@
 @php
     $page = app()->view->getSections()['page'] ?? null;
     $page = rtrim($page);
+
+    $user = Auth::user();
 @endphp
 <!DOCTYPE html>
 <html :class="{ 'dark': dark }" x-data="data()" lang="en">
@@ -40,6 +42,8 @@
       </aside>
 
       
+
+      
       <!-- Mobile sidebar -->
       <!-- Backdrop -->
       <div
@@ -71,6 +75,7 @@
           <div
             class="container flex items-center justify-between h-full px-6 mx-auto text-orange-600 dark:text-orange-300"
           >
+          <h1 class="text-center w-full font-bold">Admin Dashboard</h1>
             <!-- Mobile hamburger -->
             <button
               class="p-1 -ml-1 mr-5 rounded-md md:hidden focus:outline-none focus:shadow-outline-orange"
@@ -247,6 +252,21 @@
                     class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700"
                     aria-label="submenu"
                   >
+                   <li class="flex">
+                      <a
+                        class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+                        href="/logout"
+                      >
+                        <i class="fas fa-user mr-3 w-4 h-4"></i>
+                          <path
+                            d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                          ></path>
+                        </svg>
+                        <span>@php
+                          echo $user->name;
+                        @endphp</span>
+                      </a>
+                    </li>
                     <li class="flex">
                       <a
                         class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
